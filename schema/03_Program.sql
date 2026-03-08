c1f3efc7b7b074c83341f05a9255418d4921b54c
Anuj Sharma <94034658+Anuj-Sharma01@users.noreply.github.com>
2026-03-08 19:01:24 +0530

    Create Program table with Dept_ID foreign key reference

---
+++ schema/03_Program.sql
@@ -0,0 +1,10 @@
+create table Program(
+  Program_ID int primary key,
+  Program_Name varchar(30) not null,
+  Sanctioned_Intake int,
+  Program_Duration int not null,
+  Dept_ID int,
+  Foreign Key(Dept_ID) References Department(Dept_ID)
+    on delete cascade
+    on update cascade
+)
