43e95660f04dfa515641bad685cb7fa884a43922
Anuj Sharma <94034658+Anuj-Sharma01@users.noreply.github.com>
2026-03-15 23:05:46 +0530

    Update 03_Program.sql

--- schema/03_Program.sql
+++ schema/03_Program.sql
@@ -1,9 +1,12 @@
 create table Program(
   Program_ID int primary key,
-  Program_Name varchar(30) not null,
+  Program_Name varchar(50) not null,
   Sanctioned_Intake int,
   Program_Duration int not null,
   Dept_ID int,
+
+  check(Sanctioned_Intake > 0),
+  
   Foreign Key(Dept_ID) References Department(Dept_ID)
     on delete cascade
     on update cascade
