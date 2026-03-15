b99994197278ed699f79c4ed41f6be4964f075e6
Anuj Sharma <94034658+Anuj-Sharma01@users.noreply.github.com>
2026-03-15 23:07:41 +0530

    Added Pan.no check
    Updated Pan_no to be unique and not null. Increased Specialization length and made Date_of_Joining not null.

--- schema/04_Faculty_Details.sql
+++ schema/04_Faculty_Details.sql
@@ -1,8 +1,10 @@
 create table Faculty_Details(
   Faculty_ID int primary key,
   Faculty_Name varchar(50) not null,
-  Pan_no varchar(10),
+  Pan_no varchar(10) unique not null,
   Highest_Degree varchar(20) not null,
-  Specialization varchar(20),
-  Date_of_Joining date
+  Specialization varchar(50),
+  Date_of_Joining date not null,
+
+  check(Pan_no REGEXP '^[A-Z]{5}[0-9]{4}[A-Z]{1}$')
 );
